77947662d881dcf091bd146576c4028c6e3f1d9a
Natnipa S <s.natnipa@gmail.com>
2025-01-07 20:07:13 +0700

    feat: change the color scheme

--- src/theme/palette.rs
+++ src/theme/palette.rs
@@ -1,10 +1,10 @@
 use bevy::prelude::*;
 
-pub const BUTTON_HOVERED_BACKGROUND: Color = Color::srgb(0.186, 0.328, 0.573);
-pub const BUTTON_PRESSED_BACKGROUND: Color = Color::srgb(0.286, 0.478, 0.773);
+pub const BUTTON_HOVERED_BACKGROUND: Color = Color::srgb(0.455, 0.573, 0.184);
+pub const BUTTON_PRESSED_BACKGROUND: Color = Color::srgb(0.506, 0.773, 0.286);
 
-pub const BUTTON_TEXT: Color = Color::srgb(0.925, 0.925, 0.925);
-pub const LABEL_TEXT: Color = Color::srgb(0.867, 0.827, 0.412);
+pub const BUTTON_TEXT: Color = Color::srgb(0.867, 0.827, 0.412);
+pub const LABEL_TEXT: Color = Color::srgb(0., 0., 0.);
 pub const HEADER_TEXT: Color = Color::srgb(0.867, 0.827, 0.412);
 
-pub const NODE_BACKGROUND: Color = Color::srgb(0.286, 0.478, 0.773);
+pub const NODE_BACKGROUND: Color = Color::srgb(0.455, 0.573, 0.184);
